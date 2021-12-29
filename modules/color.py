04b4639bbfeac60fd1939332cdb185fdbb2279b9
itsbadr <badrkmc@gmail.com>
2021-12-29 13:50:25 +0500

    create session token for MaaldarSession

--- modules/color.py
+++ modules/color.py
@@ -5,6 +5,8 @@ from discord.commands import permissions, Option
 from util import configuration, check_if_user_exists
 from main import maaldar
 
+import uuid
+import asyncio
 import sqlite3
 
 
@@ -12,21 +14,63 @@ class Color(commands.Cog):
     connection = sqlite3.connect("maaldar.db")
     cursor = connection.cursor()
 
+    @staticmethod
+    async def create_session(ctx):
+        session = uuid.uuid4().hex
+        Color.cursor.execute(
+            "INSERT INTO MaaldarSession VALUES (?, ?)", (
+                ctx.author.id, session)
+        )
+        Color.connection.commit()
+        await ctx.respond("Created session, please check your DM")
+
+        try:
+            await ctx.author.send("Session created ✨\n"
+                                  f"> http://127.0.0.1:3000/{session}")
+            """Wait for 1 hour and delete session"""
+            await asyncio.sleep(3600)
+            Color.cursor.execute(
+                "DELETE FROM MaaldarSession WHERE user_id = ?", (
+                    ctx.author.id, )
+            )
+            Color.connection.commit()
+
+        except discord.Forbidden:
+            await ctx.respond("Please enable your DMs")
+            Color.cursor.execute(
+                "DELETE FROM MaaldarSession WHERE user_id = ?", (
+                    ctx.author.id, )
+            )
+            Color.connection.commit()
+
     def __init__(self, bot):
         self.bot = bot
 
     @maaldar.command()
     @permissions.has_any_role(*configuration["role_ids"])
-    async def color(ctx, new_color: Option(str, "New color for your role (e.g #000000)", required=True)):
+    async def color(ctx, new_color: Option(str, "New color for your role (e.g #000000)", required=False)):
         """Sets a new color for your role"""
-
-        new_color = new_color[1:] if new_color.startswith("#") else new_color
 
         maaldar_user = check_if_user_exists(Color.cursor, ctx.author.id)
         if maaldar_user is None:
             await ctx.respond("You do not have a role yet.\n"
                               "> Make one by typing `/maaldar create`")
             return
+
+        if not new_color:
+            Color.cursor.execute(
+                "SELECT * FROM MaaldarSession WHERE user_id = ?", (ctx.author.id, ))
+            maaldar_session = Color.cursor.fetchone()
+            if not maaldar_session:
+                asyncio.ensure_future(
+                    Color.create_session(ctx))
+
+                return
+
+            await ctx.respond(f"Session already exists {maaldar_session[1]}")
+            return
+
+        new_color = new_color[1:] if new_color.startswith("#") else new_color
 
         try:
             new_color = int(new_color, 16)
