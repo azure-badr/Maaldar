921a3e1878126dd9492d4c9baa04d0f5a6c334ed
Badr <badrkmc@gmail.com>
2022-10-13 14:49:52 +0500

    added attachment option to icon command

--- modules/icon.py
+++ modules/icon.py
@@ -5,14 +5,26 @@ import discord
 import aiohttp
 
 class Icon:
-	async def icon(interaction: discord.Interaction, url: str = None) -> None:
+	async def icon(interaction: discord.Interaction, attachment: discord.Attachment = None, url: str = None) -> None:
 		await interaction.response.defer()
 		maaldar_user = get_maaldar_user(interaction.user.id)
 		role: discord.Role = interaction.guild.get_role(int(maaldar_user[1]))
 
-		if not url:
+		if not url and not attachment:
 			await role.edit(display_icon=None)
 			await interaction.followup.send("Role icon removed 🗑️")
+			
+			return
+		
+		# If the attachment is provided, use that
+		# NOTE: If the attachment and url are both provided, the attachment is given priority
+		if attachment:
+			try:
+				await role.edit(display_icon=await attachment.read())
+				await interaction.followup.send("Role icon set ✨")
+			except Exception as error:
+				await interaction.followup.send(error)
+
 			return
 
 		if not match_url_regex(url):
@@ -21,14 +33,12 @@ class Icon:
 
 		async with aiohttp.ClientSession() as session:
 			async with session.get(url) as response:
-				if response.status == 200:
-					try:
-						await role.edit(display_icon=await response.read())
-						await interaction.followup.send("Role icon set ✨")
-						return
-
-					except Exception as error:
-						await interaction.followup.send(error)
-						return
-
-				await interaction.followup.send("Something is wrong with the website. Try a different one 👉")
+				if not response.status == 200:
+					await interaction.followup.send("Something is wrong with the website. Try a different one")
+					return
+				
+				try:
+					await role.edit(display_icon=await response.read())
+					await interaction.followup.send("Role icon set ✨")
+				except Exception as error:
+					await interaction.followup.send(error)
