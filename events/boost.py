fcafa913ead9954318223cf9bb0700d43ba4c745
Badr <badrkmc@gmail.com>
2022-10-19 23:26:41 +0500

    delete role if boosting time is less than 6 months

--- events/boost.py
+++ events/boost.py
@@ -4,12 +4,31 @@ from discord.ext import commands
 from util import configuration
 from database.db import database
 
+from datetime import timedelta
+
 class BoostEvent(commands.Cog):
+  DAYS_REQUIRED_FOR_ROLE = 180
+  
   connection = database.connection
   cursor = database.cursor
 
   def __init__(self, bot):
     self.bot = bot
+  
+  def _check_and_update_duration(self, member_id: str, boosting_since: str) -> None:
+    self.cursor.execute(f"SELECT * FROM MaaldarDuration WHERE user_id = '{member_id}'")
+    if self.cursor.fetchone() is None:
+      self.cursor.execute(
+        f"INSERT INTO MaaldarDuration VALUES ('{member_id}', '{boosting_since}')"
+      )
+      self.connection.commit()
+      return
+    
+    self.cursor.execute(
+      f"UPDATE MaaldarDuration SET boosting_since = boosting_since + '{boosting_since}' "
+      f"WHERE user_id = '{member_id}'"
+    )
+    self.connection.commit()
   
   @commands.Cog.listener()
   async def on_member_update(self, before: discord.Member, after: discord.Member):
@@ -25,23 +44,34 @@ class BoostEvent(commands.Cog):
     #   )
     #   self.connection.commit()
     
-    if before.premium_since is not None and after.premium_since is None:
-      # Setting member.premium_since.tzinfo to None to avoid naive and aware datetime comparison
-      boosting_since = before.premium_since.replace(tzinfo=None)
-      self.cursor.execute(f"SELECT * FROM MaaldarDuration WHERE user_id = '{member.id}'")
-      if self.cursor.fetchone() is None:
-        self.cursor.execute(
-          f"INSERT INTO MaaldarDuration VALUES ('{member.id}', '{boosting_since}')"
-        )
-        self.connection.commit()
-        return
-
-      self.cursor.execute(
-        f"UPDATE MaaldarDuration SET boosting_since = boosting_since + '{boosting_since}' "
-        f"WHERE user_id = '{member.id}'"
-      )
-      self.connection.commit()
+    # If member was not boosting before, return
+    if before.premium_since is None:
+      return
     
+    # If member was boosting after, return
+    if after.premium_since is not None:
+      return
+    
+    # Setting member.premium_since.tzinfo to None to avoid naive and aware datetime comparison
+    boosting_since = before.premium_since.replace(tzinfo=None)
+    self._check_and_update_duration(member.id, boosting_since)
+    
+    self.cursor.execute(f"SELECT boosting_since FROM MaaldarDuration WHERE user_id = '{member.id}'")
+    boosting_since = self.cursor.fetchone()[0] # timedelta object
+    
+    if boosting_since >= timedelta(days=self.DAYS_REQUIRED_FOR_ROLE):
+      return
+    
+    self.cursor.execute(f"SELECT role_id FROM Maaldar WHERE user_id = '{member.id}'")
+    if self.cursor.fetchone() is None:
+      return
+    
+    role_id = self.cursor.fetchone()[0]
+    role = member.guild.get_role(int(role_id))
+
+    await member.remove_roles(role)
+    self.cursor.execute(f"DELETE FROM Maaldar WHERE user_id = '{member.id}'")
+    self.connection.commit()
 
 async def setup(bot: commands.Bot):
   await bot.add_cog(BoostEvent(bot), guilds=[discord.Object(id=configuration["guild_id"])])
