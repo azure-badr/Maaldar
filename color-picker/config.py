3e10be245fdbfed36526f1e149c8030b095a7310
Badr <badrkmc@gmail.com>
2023-01-29 21:02:37 +0500

    use psycopg2_pool for db pool

--- color-picker/config.py
+++ color-picker/config.py
@@ -9,27 +9,20 @@ try:
   if os.environ["ENVIRONMENT"] == "production":
     configuration = {
 			"guild_id": int(os.environ["GUILD_ID"]),
-			"database_name": os.environ["DATABASE_NAME"],
-			"database_user": os.environ["DATABASE_USER"],
-			"database_password": os.environ["DATABASE_PASSWORD"],
-			"database_host": os.environ["DATABASE_HOST"],
-			"database_port": os.environ["DATABASE_PORT"],
+			"connection_string": os.environ["CONNECTION_STRING"],
 			"token": os.environ["TOKEN"]
     }
 except KeyError:
   pass
 
-import psycopg2
-database_name = configuration["database_name"]
-database_user = configuration["database_user"]
-database_password = configuration["database_password"]
-database_host = configuration["database_host"]
-database_port = configuration["database_port"]
+import psycopg2_pool
 
-connection_string = f"dbname={database_name} user={database_user} password={database_password} host={database_host} port={database_port}"
+pool = psycopg2_pool.ConnectionPool(minconn=5, maxconn=20, dsn=configuration["connection_string"], idle_timeout=60)
 
 def select_one(query, params):
-	with psycopg2.connect(connection_string) as connection:
-		with connection.cursor() as cursor:
+	with pool.getconn() as conn:
+		with conn.cursor() as cursor:
 			cursor.execute(query, params)
-			return cursor.fetchone()
+			result = cursor.fetchone()
+	
+	return result
