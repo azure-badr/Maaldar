5b3935b3032d83f7eccf2cc77cc8999df8182eb3
Badr <badrkmc@gmail.com>
2023-07-22 13:10:26 +0500

    Revert "better readability for image palette utils"
    This reverts commit 53df810894e4aba55f2b0d21961265fdd9d45dc5.

--- util.py
+++ util.py
@@ -66,32 +66,52 @@ def select_all(query):
 def get_maaldar_user(user_id):
   return select_one(f"SELECT * FROM Maaldar WHERE user_id = '{user_id}'")
 
-BASE_IMAGE_SIZE = (50, 50)
-CONCATENATED_IMAGE_SIZE = (BASE_IMAGE_SIZE[0] * 10, BASE_IMAGE_SIZE[1]) # width of 10 images and height of 1 image
-BORDER_RADIUS = 20
-FONT = ImageFont.load_default()
-
 def make_image(dominant_color):
-	base_image = Image.new(mode="RGBA", size=BASE_IMAGE_SIZE, color=(0, 0, 0, 0))
-	ImageDraw.Draw(base_image).rounded_rectangle((0, 0, *BASE_IMAGE_SIZE), radius=BORDER_RADIUS, fill=dominant_color)
-	return base_image
+	image = Image.new(
+		mode="RGBA", 
+		size=(50, 50),
+		color=(0, 0, 0, 0)
+	)
+	ImageDraw.Draw(image).rounded_rectangle(
+		(0, 0, 50, 50), 
+		radius=20,
+		fill=dominant_color
+	)
+	return image
 
 def concatenate_images(images):
 	# size = width of 10 images and height of 1 image
-	concatenated_image = Image.new(mode="RGBA", size=CONCATENATED_IMAGE_SIZE, color=(0, 0, 0, 0))
-	ImageDraw.Draw(concatenated_image).rounded_rectangle((0, 0, *CONCATENATED_IMAGE_SIZE), radius=BORDER_RADIUS)
+	image = Image.new(
+		mode="RGBA", 
+		size=(50 * 10, 50),
+		color=(0, 0, 0, 0)
+	)
+	ImageDraw.Draw(image).rounded_rectangle(
+		(0, 0, 500, 50), 
+		radius=20
+	)
 
 	width = 0
 	for index, image_to_paste in enumerate(images, start=1):
+		font = ImageFont.load_default()
+		font
 		draw = ImageDraw.Draw(image_to_paste)
-		draw.text((10, 10), f"{index}", font=FONT, stroke_width=1, stroke_fill="white")
-		concatenated_image.paste(image_to_paste, (width, 0))
-		width += BASE_IMAGE_SIZE[0]
+		draw.text(
+			(10, 10), 
+			f"{index}", 
+			font=font,
+			stroke_width=1, 
+			stroke_fill="white"
+		)
 
-	with io.BytesIO() as bytes_array:
-		concatenated_image.save(bytes_array, format="PNG")
-		bytes_array.seek(0)
-		return bytes_array
+		image.paste(image_to_paste, (width, 0))
+		width += 50
+
+	bytes_array = io.BytesIO()
+	image.save(bytes_array, format="PNG")
+	bytes_array.seek(0)
+
+	return bytes_array
 
 def rgb_to_hex(rgb):
 	return "%02x%02x%02x" % (rgb)
