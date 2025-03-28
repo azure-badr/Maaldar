87d47615a810e71d341df9ce7f88395f7aab729b
pbadr <badrkmc@gmail.com>
2025-03-28 11:45:00 +0500

    doc: add docs for delete_expired_session task

--- modules/maaldar.py
+++ modules/maaldar.py
@@ -12,6 +12,8 @@ from modules.icon import Icon
 
 from util import get_maaldar_user, configuration, select_one
 
+from psycopg2.errors import UndefinedFunction
+
 class Maaldar(commands.GroupCog, name="maaldar"):
   def __init__(self, bot: commands.Bot) -> None:
     self.bot = bot
@@ -141,6 +143,7 @@ class Maaldar(commands.GroupCog, name="maaldar"):
     description="Positions your role to other Maaldar roles"
   )
   @app_commands.checks.has_any_role(*configuration["role_ids"])
+  @has_custom_role()
   async def _position(self, interaction: discord.Interaction) -> None:
     await Role.position(interaction=interaction)
   
@@ -183,7 +186,13 @@ class Maaldar(commands.GroupCog, name="maaldar"):
   
   @tasks.loop(seconds=3600)
   async def delete_sessions(self):
-    select_one("SELECT delete_expired_sessions();")
+    print("Deleting expired sessions for color-picker...")
+    try:
+      select_one("SELECT delete_expired_sessions();")
+    except UndefinedFunction as error:
+      print("[!] delete_expired_sessions() function does not exist on your database.")
+      print("[!] Run maaldar-db/maaldar_session.sql on your database.")
+      print(error)
   
   @delete_sessions.before_loop
   async def before_delete_sessions(self):
